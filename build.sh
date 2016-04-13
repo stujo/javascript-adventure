471c700022ab59fd83435a61b0f4154dd8a539a7
Stujo <email": null>
2016-04-13 12:09:09 -0700

    Use normalize.css on cdn

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo 'Building index.html ...'
-node ./node_modules/markdown-to-html/bin/github-markdown README.md > index.html
+node ./node_modules/markdown-to-html/bin/github-markdown -s https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css README.md > index.html
 
