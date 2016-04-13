6249851738f8ad361d501532f61e1a96bc2c5041
Stujo <email": null>
2016-04-13 10:42:13 -0700

    Add extra cities

--- solution/app.js
+++ solution/app.js
@@ -4,14 +4,25 @@ var currentIndex = 0
 var worldTourPlaces = [
     { 
     	name: "London", 
-    	description: "Capital of England", 
+    	description: "London is a leading global city, with strengths in the arts, commerce, education, entertainment, fashion, finance, healthcare, media, professional services, research and development, tourism, and transport all contributing to its prominence. It is one of the world's leading financial centres and has the fifth-or sixth-largest metropolitan area GDP in the world depending on measurement. London is a world cultural capital. It is the world's most-visited city as measured by international arrivals and has the world's largest city airport system measured by passenger traffic.", 
     	imageUrl: "http://www.suffolkcoastsailing.co.uk/_wp/wp-content/uploads/2014/04/tower-bridge1.jpg" 
     },
     { 
-    	name: "Paris", 
-    	description: "Paris was founded in the 3rd century BC by a Celtic people called the Parisii, who gave the city its name. By the 12th century, Paris was the largest city in the western world, a prosperous trading centre, and the home of the University of Paris, one of the first in Europe. In the 18th century, it was the centre stage for the French Revolution, and became an important centre of finance, commerce, fashion, science, and the arts, a position it still retains today. Since the 19th century, the built-up area of Paris has grown far beyond its administrative borders.", 
-    	imageUrl: "http://travelnoire.com/wp-content/uploads/2014/02/Eiffel-Tower-Paris-France.jpg" 
+        name: "Paris", 
+        description: "Paris was founded in the 3rd century BC by a Celtic people called the Parisii, who gave the city its name. By the 12th century, Paris was the largest city in the western world, a prosperous trading centre, and the home of the University of Paris, one of the first in Europe. In the 18th century, it was the centre stage for the French Revolution, and became an important centre of finance, commerce, fashion, science, and the arts, a position it still retains today. Since the 19th century, the built-up area of Paris has grown far beyond its administrative borders.", 
+        imageUrl: "http://travelnoire.com/wp-content/uploads/2014/02/Eiffel-Tower-Paris-France.jpg" 
+    },
+    { 
+        name: "Vienna", 
+        description: "Vienna is the capital and largest city of Austria, and one of the nine states of Austria. Vienna is Austria's primary city, with a population of about 1.8 million.", 
+        imageUrl: "https://media-cdn.tripadvisor.com/media/photo-s/03/9b/30/4e/vienna.jpg" 
+    },
+    { 
+        name: "Tai Pai", 
+        description: "The city proper is home to an estimated population of 2,693,672 in 2009, forming the core part of the Taipei–Keelung metropolitan area which includes the nearby cities of New Taipei and Keelung with a population of 6,900,273, the 40th most-populous urban area in the world. The name Taipei can refer either to the whole metropolitan area or the city proper.", 
+        imageUrl: "http://www.outthere.travel/wp-content/uploads/2015/10/OTT-Taipei.jpg" 
     }
+
 ];
 
 // function printCurrentLocationName() {
